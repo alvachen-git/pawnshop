7199802abc0c8c9ad165df99f41c06b9a2b77748
alvachen <alvachenart@163.com>
2026-09-23 22:05:23 +0800

    test: verify v38 integration with latest main and legacy entries

--- tests/appraisal_release_entries.gd
+++ tests/appraisal_release_entries.gd
@@ -2,7 +2,8 @@ extends SceneTree
 
 func _initialize() -> void:
 	var cases := {
-		"start": "named_wealthy_ten",
+		"start": "pearl_market_ten",
+		"start_named_wealthy_v37": "named_wealthy_ten",
 		"start_mirror_dream_call_v29": "mirror_dream_call_ten",
 		"start_social_relations_v27": "social_relations_ten",
 		"start_fan_condition_v29": "fan_condition_ten",
